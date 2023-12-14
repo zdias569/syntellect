ce5b71792f1863fe1316d7f2c062fd631c6e8781
zdias569 <68955373+zdias569@users.noreply.github.com>
2023-12-14 17:16:20 +0300

    disable double countries

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,29 +1,41 @@
 import countries from "./countries.json";
 
 export interface CountryInfo {
-  name: string;
-  fullName: string;
-  flag: string;
+    name: string;
+    fullName: string;
+    flag: string;
 }
 
 export function getCountryByName(countryName: string): Promise<CountryInfo[]> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, getRandom(100, 800));
-  }).then(() => {
-    if (typeof countryName !== "string" || !countryName) {
-      return [];
-    }
+    return new Promise((resolve) => {
+        setTimeout(resolve, getRandom(100, 800));
+    }).then(() => {
+        if (typeof countryName !== "string" || !countryName) {
+            return [];
+        }
 
-    const searchText = countryName.toLocaleLowerCase();
+        const searchText = countryName.toLocaleLowerCase();
+    
+        const filteredCountries = countries.filter(
+            (x) =>
+                x.name.toLocaleLowerCase().startsWith(searchText) ||
+                x.fullName.toLocaleLowerCase().startsWith(searchText)
+        );
 
-    return countries.filter(
-      (x) =>
-        x.name.toLocaleLowerCase().startsWith(searchText) ||
-        x.fullName.toLocaleLowerCase().startsWith(searchText)
-    );
-  });
+        const uniqueCountries = filteredCountries.reduce((accumulator: CountryInfo[], currentValue: CountryInfo) => {
+            const existingCountry = accumulator.find((item) => item.name === currentValue.name);
+
+            if (!existingCountry) {
+                accumulator.push(currentValue);
+            }
+
+            return accumulator;
+        }, []);
+
+        return uniqueCountries;
+    });
 }
 
 function getRandom(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
